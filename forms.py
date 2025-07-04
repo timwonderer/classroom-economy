322a11567047d6c0243de9445b4082e272c21373
Timothy Chang <76604824+timwonderer@users.noreply.github.com>
2025-07-04 15:08:23 -0700

    feat: add SystemAdminLoginForm and SystemAdminInviteForm for improved admin functionality

--- forms.py
+++ forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
 from wtforms import HiddenField
+from wtforms import SubmitField
 
 class AdminSignupForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -11,3 +12,13 @@ class AdminTOTPConfirmForm(FlaskForm):
     totp_code = StringField('TOTP Code', validators=[DataRequired()])
     username = HiddenField(validators=[DataRequired()])
     invite_code = HiddenField(validators=[DataRequired()])
+
+class SystemAdminLoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    totp_code = StringField('TOTP Code', validators=[DataRequired()])
+    submit = SubmitField('Login')
+class SystemAdminInviteForm(FlaskForm):
+    code = StringField('Custom Code')
+    expiry_days = StringField('Expiry Days')
+    expires_at = StringField('Expires At')  # Added to match app.py usage
+    submit = SubmitField('Generate Invite Code')
