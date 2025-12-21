1cd5e1f2c98110603425c93feb0b9e7eba033206
Timothy Chang <76604824+timwonderer@users.noreply.github.com>
2025-12-21 11:39:16 -0800

    Merge migration heads and restore join_code migration for student_blocks table

--- migrations/versions/a1b2c3d4e5f8_add_join_code_to_student_blocks.py
+++ migrations/versions/a1b2c3d4e5f8_add_join_code_to_student_blocks.py
@@ -1,6 +1,6 @@
 """Add join_code to student_blocks table
 
-Revision ID: a1b2c3d4e5f6
+Revision ID: a1b2c3d4e5f8
 Revises: z2a3b4c5d6e7
 Create Date: 2025-12-21 19:00:00.000000
 
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'a1b2c3d4e5f6'
+revision = 'a1b2c3d4e5f8'
 down_revision = 'z2a3b4c5d6e7'
 branch_labels = None
 depends_on = None
