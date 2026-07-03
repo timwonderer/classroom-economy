5626b93eba89af6cafc2f051cc50c425a88040e4
Timothy Chang <76604824+timwonderer@users.noreply.github.com>
2026-07-03 15:09:36 -0700

    Restore merge head for student drop migrations

---
+++ migrations/versions/0b1c2d3e4f5a_merge_student_table_drop_heads.py
@@ -0,0 +1,19 @@
+"""Merge students and student_blocks drop heads.
+
+Revision ID: 0b1c2d3e4f5a
+Revises: 8b1bc35a7d58, 9c1d2e3f4a5b
+Create Date: 2026-07-03 22:06:54.000000
+"""
+
+revision = "0b1c2d3e4f5a"
+down_revision = ("8b1bc35a7d58", "9c1d2e3f4a5b")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
